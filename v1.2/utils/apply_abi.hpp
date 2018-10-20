95b255d643b50c4bd51d0815025c545c1722f7bf
bagaking <zfaustk@qq.com>
2018-10-20 20:28:49 +0800

    feat: apply_abi.hpp

---
+++ v1.2/utils/apply_abi.hpp
@@ -0,0 +1,22 @@
+
+#define EOSIO_ABI_EX(TYPE, MEMBERS)                                                                \
+  extern "C" {                                                                                     \
+  void apply(uint64_t receiver, uint64_t code, uint64_t action) {                                  \
+    if (action == N(onerror)) {                                                                    \
+      /* onerror is only valid if it is for the "eosio" code account and authorized by "eosio"'s   \
+       * "active permission */                                                                     \
+      eosio_assert(code == N(eosio),                                                               \
+                   "onerror action's are only valid from the \"eosio\" system account");           \
+    }                                                                                              \
+    auto self = receiver;                                                                          \
+                                                                                                   \
+    bool valid_internal_actions = code == self &&                                                  \
+      action != N(ontransfer);     /* put all external actions separated by && */                    \
+                                                                                                   \
+    if (valid_internal_actions || code == N(eosio.token) || action == N(onerror)) {                \
+      TYPE thiscontract(self);                                                                     \
+      switch (action) { EOSIO_API(TYPE, MEMBERS) }                                                 \
+      /* does not allow destructor of thiscontract to run: eosio_exit(0); */                       \
+    }                                                                                              \
+  }                                                                                                \
+  }
